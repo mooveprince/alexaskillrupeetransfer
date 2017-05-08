aae6b2d858bd56a8de4d2187b1768a3a981e6698
mooveprince <mooveprince@gmail.com>
2017-05-08 00:03:13 -0400

    simple unit

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 var Alexa = require ('alexa-sdk');
 var Utility = require ('./utility.js');
+
+var handlers = {
+
+    "AMAZON.StopIntent": function () {
+        var speechText = "Goodbye";
+        this.emit(':tell', speechText);
+    },
+ 
+    "AMAZON.CancelIntent": function () {
+        var speechText = "Goodbye";
+        this.emit(':tell', speechText);
+    },
+    
+    "AMAZON.HelpIntent" : function () {
+        var speechText = "Here are somethings you can say: ";
+        speechText += " What's' today's transfer rate ? ";
+        speechText += " Current news in Tech Crunch.";
+        speechText += " Trending topics in Tech Meme";
+
+        this.emit(':ask', speechText, speechText);
+        
+    }
+}
+
+exports.handler = function (event, context) {
+    var alexa = Alexa.handler(event, context);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
